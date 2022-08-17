daeb4eef68b323ac62e2e2487463631073652217
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-17 08:03:59 -0300

    ensure Service

--- src/domain/features/facebook-authentication.ts
+++ src/domain/features/facebook-authentication.ts
@@ -1,14 +1,14 @@
-import { AccessToken } from '@/domain/models';
-import { AuthenticationError } from '@/domain/errors';
+import { AccessToken } from '@/domain/models'
+import { AuthenticationError } from '@/domain/errors'
 
 export interface FacebookAuthentication {
-    perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>;
+  perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
 }
 
-namespace FacebookAuthentication {
-    export type Params = {
-        token: string;
-    };
+export namespace FacebookAuthentication {
+  export type Params = {
+    token: string
+  }
 
-    export type Result = AccessToken | AuthenticationError;
+  export type Result = AccessToken | AuthenticationError
 }
