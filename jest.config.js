7929ff0dca99c1d845d656942467bde88ec82490
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-19 11:20:04 -0300

    ignore index files and main layer from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'babel',
-  moduleNameMapper: {
-    '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1',
-  },
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  transform: {
-    '\\.ts$': 'ts-jest',
-  },
-  clearMocks: true,
-}
+    collectCoverage: true,
+    collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main**', '!<rootDir>/src/**/index.ts'],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    moduleNameMapper: {
+        '@/tests/(.+)': '<rootDir>/tests/$1',
+        '@/(.+)': '<rootDir>/src/$1',
+    },
+    roots: ['<rootDir>/src', '<rootDir>/tests'],
+    transform: {
+        '\\.ts$': 'ts-jest',
+    },
+    clearMocks: true,
+};
