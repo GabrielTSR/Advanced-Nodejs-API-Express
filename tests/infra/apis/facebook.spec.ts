9f4f0350732659d630307ca523876eac13a196ff
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-19 09:09:45 -0300

    ensure api get app token with correct URL

---
+++ tests/infra/apis/facebook.spec.ts
@@ -0,0 +1,32 @@
+import { mock } from 'jest-mock-extended';
+import { LoadFacebookUserApi } from '@/data/contracts/apis/facebook';
+class FacebookApi {
+    private readonly baseUrl = 'https://graph.facebook.com';
+    constructor(private readonly httpClient: HttpGetClient) {}
+    async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
+        await this.httpClient.get({ url: `${this.baseUrl}/oauth/access_token` });
+    }
+}
+
+interface HttpGetClient {
+    get: (params: HttpGetClient.Params) => Promise<any>;
+}
+
+namespace HttpGetClient {
+    export type Params = {
+        url: string;
+    };
+}
+
+describe('FacebookApi', () => {
+    it('should get app token', async () => {
+        const httpClient = mock<HttpGetClient>();
+        const sut = new FacebookApi(httpClient);
+
+        await sut.loadUser({ token: 'any_client_token' });
+
+        expect(httpClient.get).toHaveBeenCalledWith({
+            url: 'https://graph.facebook.com/oauth/access_token',
+        });
+    });
+});
