e5b3401d71856e37dd93af329d094f23ec226e87
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-20 19:28:59 -0300

    ensure PgUserAccountRepository loads account if email exists

--- src/data/contracts/repos/user-account.ts
+++ src/data/contracts/repos/user-account.ts
@@ -1,29 +1,29 @@
 export interface LoadUserAccountRepository {
-    load: (params: LoadUserAccountRepository.Params) => Promise<LoadUserAccountRepository.Result>;
+    load: (params: LoadUserAccountRepository.Params) => Promise<LoadUserAccountRepository.Result>
 }
 
 export namespace LoadUserAccountRepository {
     export type Params = {
-        email: string;
+        email: string
     }
     export type Result =
         | undefined
         | {
-              id: string;
-              name?: string;
+              id: string
+              name?: string
           }
 }
 
 export interface SaveFacebookAccountRepository {
-    saveWithFacebook: (params: SaveFacebookAccountRepository.Params) => Promise<SaveFacebookAccountRepository.Result>;
+    saveWithFacebook: (params: SaveFacebookAccountRepository.Params) => Promise<SaveFacebookAccountRepository.Result>
 }
 
 export namespace SaveFacebookAccountRepository {
     export type Params = {
-        id?: string;
-        email: string;
-        name: string;
-        facebookId: string;
+        id?: string
+        email: string
+        name: string
+        facebookId: string
     }
 
     export type Result = { id: string }
