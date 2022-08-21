afb3d051e2df6499f248ac8799b048860cdabcfa
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-21 13:14:09 -0300

    isolate SUT creation

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,4 +1,4 @@
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 import { FacebookAuthentication } from '@/domain/features'
 class FacebookLoginController {
     constructor(private readonly facebookAuthentication: FacebookAuthentication) {}
@@ -18,19 +18,24 @@ type HttpResponse = {
 }
 
 describe('FacebookLoginController', () => {
-    it('should return 400 if token is empty', async () => {
-        const facebookAuth = mock<FacebookAuthentication>()
-        const sut = new FacebookLoginController(facebookAuth)
+    let sut: FacebookLoginController
+    let facebookAuth: MockProxy<FacebookAuthentication>
 
+    beforeAll(() => {
+        facebookAuth = mock<FacebookAuthentication>()
+    })
+
+    beforeEach(() => {
+        sut = new FacebookLoginController(facebookAuth)
+    })
+
+    it('should return 400 if token is empty', async () => {
         const httpResponse = await sut.handle({ token: '' })
 
         expect(httpResponse).toEqual({ statusCode: 400, data: new Error('Missing token') })
     })
 
     it('should return 400 if token is null', async () => {
-        const facebookAuth = mock<FacebookAuthentication>()
-        const sut = new FacebookLoginController(facebookAuth)
-
         const httpResponse = await sut.handle({ token: null })
 
         expect(httpResponse).toEqual({ statusCode: 400, data: new Error('Missing token') })
@@ -46,9 +51,6 @@ describe('FacebookLoginController', () => {
     })
 
     it('should call FacebookAuthentication with correct params', async () => {
-        const facebookAuth = mock<FacebookAuthentication>()
-        const sut = new FacebookLoginController(facebookAuth)
-
         await sut.handle({ token: 'any_token' })
 
         expect(facebookAuth.perform).toHaveBeenCalledWith({ token: 'any_token' })
