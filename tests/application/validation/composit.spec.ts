aa1e3bebb2ad422ce7af1bfa0b2e65468cd7265b
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-21 18:20:02 -0300

    isolate SUT creation

---
+++ tests/application/validation/composit.spec.ts
@@ -0,0 +1,36 @@
+import { mock, MockProxy } from 'jest-mock-extended'
+interface Validator {
+    validate: () => Error | undefined
+}
+
+class ValidationComposite {
+    constructor(private readonly validators: Validator[]) {}
+
+    validate(): undefined {
+        return undefined
+    }
+}
+
+describe('ValidationComposite', () => {
+    let sut: ValidationComposite
+    let validator1: MockProxy<Validator>
+    let validator2: MockProxy<Validator>
+    let validators: Validator[]
+
+    beforeAll(() => {
+        validator1 = mock<Validator>()
+        validator1.validate.mockReturnValue(undefined)
+        validator2 = mock<Validator>()
+        validator2.validate.mockReturnValue(undefined)
+        validators = [validator1, validator2]
+    })
+    beforeEach(() => {
+        sut = new ValidationComposite(validators)
+    })
+
+    it('should return undefined if all Validators return undefined', () => {
+        const error = sut.validate()
+
+        expect(error).toBeUndefined()
+    })
+})
