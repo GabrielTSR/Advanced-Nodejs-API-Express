b5844ef7c1505006394820c8533dcadaca35d07d
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-20 22:03:54 -0300

    make PgUserAccountRepository implements SaveFacebookAccountRepository interface

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -6,7 +6,7 @@ type LoadParams = LoadUserAccountRepository.Params
 type LoadResult = LoadUserAccountRepository.Result
 type SaveParams = SaveFacebookAccountRepository.Params
 type SaveResult = SaveFacebookAccountRepository.Result
-export class PgUserAccountRepository implements LoadUserAccountRepository {
+export class PgUserAccountRepository implements LoadUserAccountRepository, SaveFacebookAccountRepository {
     private readonly pgUserRepo = getRepository(PgUser)
 
     async load(params: LoadParams): Promise<LoadResult> {
