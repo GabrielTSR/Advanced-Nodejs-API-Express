c4f048c88a1d928e4d24ec566c94ab5e2e571715
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-22 11:32:40 -0300

    11:30

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,16 @@
-import 'reflect-metadata'
+// import 'reflect-metadata'
+// import express, { json } from 'express'
+// import cors from 'cors'
+// import './config/module-alias'
+
+// const app = express()
+// app.use(cors())
+// app.use(json())
+// app.use((req, res, next) => {
+
+// }))
+
+// const port = 3333
+// app.listen(port, () => {
+//     console.log('Server running at http://localhost:%d', port)
+// })
