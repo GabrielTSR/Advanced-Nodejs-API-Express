82eeed2ee1c540cfbc1a0cbfe94d0c23528f20b5
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-25 11:12:08 -0300

    ensure authentication middleware returns 200 if authorization header is valid

--- tests/main/middlewares/authentication.spec.ts
+++ tests/main/middlewares/authentication.spec.ts
@@ -1,6 +1,7 @@
-import { ForbiddenError } from '@/application/errors'
 import { app } from '@/main/config/app'
+import { makeJwtTokenHandler } from '@/main/factories/crypto/jwt-token-handler'
 import { auth } from '@/main/middlewares'
+import { ForbiddenError } from '@/application/errors'
 
 import request from 'supertest'
 
@@ -15,13 +16,21 @@ describe('Authentication Middleware', () => {
     })
 
     it('Should return 200 if authorization header is valid', async () => {
+        const jwtTokenHandler = makeJwtTokenHandler()
+        const authorization = await jwtTokenHandler.generateToken({
+            expirationInMs: 1000000,
+            key: 'any_user_id',
+        })
+
         app.get('/fake_route', auth, (req, res) => {
             res.json(req.locals)
         })
 
-        const { status, body } = await request(app).get('/fake_route')
+        const { status, body } = await request(app)
+            .get('/fake_route')
+            .set({ authorization })
 
-        expect(status).toBe(403)
-        expect(body.error).toBe(new ForbiddenError().message)
+        expect(status).toBe(200)
+        expect(body).toEqual({ userId: 'any_user_id' })
     })
 })
