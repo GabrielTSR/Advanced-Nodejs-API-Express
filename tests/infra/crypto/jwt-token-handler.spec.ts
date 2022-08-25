cbbbf511ab9b27193109b99867ddbb760680e28d
GabrielTSR <gabrielalvesrocha22@gmail.com>
2022-08-25 06:49:48 -0300

    organize code

--- tests/infra/crypto/jwt-token-handler.spec.ts
+++ tests/infra/crypto/jwt-token-handler.spec.ts
@@ -3,48 +3,103 @@ import { JwtTokenHandler } from '@/infra/crypto'
 
 jest.mock('jsonwebtoken')
 
-describe('JwtTokenGenerator', () => {
+describe('JwtTokenHandler', () => {
     let sut: JwtTokenHandler
     let fakeJwt: jest.Mocked<typeof jwt>
+    let secret: string
 
     beforeAll(() => {
+        secret = 'any_secret'
         fakeJwt = jwt as jest.Mocked<typeof jwt>
-        fakeJwt.sign.mockImplementation(() => 'any_token')
     })
 
     beforeEach(() => {
-        sut = new JwtTokenHandler('any_secret')
+        sut = new JwtTokenHandler(secret)
     })
 
-    it('should call sign with correct params', async () => {
-        await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+    describe('generateToken', () => {
+        let key: string
+        let expirationInMs: number
+        let token: string
 
-        expect(fakeJwt.sign).toHaveBeenCalledWith(
-            { key: 'any_key' },
-            'any_secret',
-            { expiresIn: 1 }
-        )
+        beforeAll(() => {
+            key = 'any_key'
+            token = 'any_token'
+            expirationInMs = 1000
+            fakeJwt.sign.mockImplementation(() => token)
+        })
+
+        it('should call sign with correct params', async () => {
+            await sut.generateToken({ key, expirationInMs })
+
+            expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, {
+                expiresIn: 1,
+            })
+        })
+
+        it('should return a token', async () => {
+            const generatedToken = await sut.generateToken({
+                key,
+                expirationInMs,
+            })
+
+            expect(generatedToken).toBe(token)
+        })
+
+        it('should rethrow if sign throws', async () => {
+            fakeJwt.sign.mockImplementationOnce(() => {
+                throw new Error('token_error')
+            })
+
+            const result = sut.generateToken({
+                key,
+                expirationInMs,
+            })
+
+            await expect(result).rejects.toThrow(new Error('token_error'))
+        })
     })
 
-    it('should return a token', async () => {
-        const token = await sut.generateToken({
-            key: 'any_key',
-            expirationInMs: 1000,
+    describe('generateToken', () => {
+        let key: string
+        let expirationInMs: number
+        let token: string
+
+        beforeAll(() => {
+            key = 'any_key'
+            token = 'any_token'
+            expirationInMs = 1000
+            fakeJwt.sign.mockImplementation(() => token)
         })
 
-        expect(token).toBe('any_token')
-    })
+        it('should call sign with correct params', async () => {
+            await sut.generateToken({ key, expirationInMs })
 
-    it('should rethrow if sign throws', async () => {
-        fakeJwt.sign.mockImplementationOnce(() => {
-            throw new Error('token_error')
+            expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, secret, {
+                expiresIn: 1,
+            })
         })
 
-        const result = sut.generateToken({
-            key: 'any_key',
-            expirationInMs: 1000,
+        it('should return a token', async () => {
+            const generatedToken = await sut.generateToken({
+                key,
+                expirationInMs,
+            })
+
+            expect(generatedToken).toBe(token)
         })
 
-        await expect(result).rejects.toThrow('token_error')
+        it('should rethrow if sign throws', async () => {
+            fakeJwt.sign.mockImplementationOnce(() => {
+                throw new Error('token_error')
+            })
+
+            const result = sut.generateToken({
+                key,
+                expirationInMs,
+            })
+
+            await expect(result).rejects.toThrow(new Error('token_error'))
+        })
     })
 })
